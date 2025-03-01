101f31c3fb00a56e663f5c19e02a846b273cb113
Noah S <ngsorenson@gmail.com>
2025-02-28 21:13:18 -0700

    Major fix

--- brickfish/src/pages/MainFooter.jsx
+++ brickfish/src/pages/MainFooter.jsx
@@ -1,7 +1,7 @@
 const MainFooter = () => {
     return (
         <footer className="footer bg-dark text-light text-center p-1">
-            <p>&copy; 2021 Brickfish</p>
+            <p>&copy; 2025 Brickfish</p>
         </footer>
     )
 }
