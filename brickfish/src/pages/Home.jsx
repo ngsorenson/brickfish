3f26ba02b2efa176f9557be35c9fdd0b7da0ef27
Noah S <ngsorenson@gmail.com>
2025-02-28 22:24:11 -0700

    Undocumented changes

--- brickfish/src/pages/Home.jsx
+++ brickfish/src/pages/Home.jsx
@@ -1,15 +1,25 @@
 import '../App.css';
 import MainLayout from '../layout/MainLayout';
+import { Chessboard } from 'react-chessboard';
+import { useEffect } from 'react';
+import { Chess } from 'chess.js';
+
+const ID = 'placeholder'
 
 const Home = () => {
 
     return (
         <MainLayout title="Brickfish Home">
-            <div>
-                <h1>Brickfish home</h1>
-            </div>
+            <h1>Home</h1>
+
         </MainLayout>
     )
+
+    // function onPieceClick
+
+    function startGame() {
+        alert('startGame')
+    }
 }
 
 export default Home
